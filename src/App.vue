ef69b68cdfd46db07795d4c6ed889a946be5f6b3
zacks <bluemilk1995@gmail.com>
2017-08-28 15:31:03 +0800

    ver.1.0
    test1

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Nav></Nav>
+    <Jum></Jum>
+    <Card></Card>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav'
+import Jum from '@/components/Jumbotron'
+import Card from '@/components/Card'
+
+export default {
+  name: 'app',
+  components: {
+    Nav,
+    Jum,
+    Card
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+
+</style>
